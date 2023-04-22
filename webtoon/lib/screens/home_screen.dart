d7664c7d8b5e4aaaae245c5bd5a4db9252dcccba
yoon <leeyt1201@gmail.com>
2023-04-23 05:51:13 +0900

    feat: implemented app bar

---
+++ webtoon/lib/screens/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: const Text(
+          "Today's toons",
+          style: TextStyle(fontSize: 26, fontWeight: FontWeight.w600),
+        ),
+      ),
+    );
+  }
+}
