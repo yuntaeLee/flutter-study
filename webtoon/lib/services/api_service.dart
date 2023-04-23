69941b93babaef5a86459fda929e89f62ecaa20e
yoon <leeyt1201@gmail.com>
2023-04-23 09:36:50 +0900

    feat: implemented get webtoon detail api by webtoon id

--- webtoon/lib/services/api_service.dart
+++ webtoon/lib/services/api_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:webtoon/models/webtoon_model.dart';
+import 'package:webtoon/models/webtoon_detail_model.dart';
 
 class ApiService {
   static const String baseUrl =
@@ -15,6 +16,18 @@ class ApiService {
     if (response.statusCode == 200) {
       final List<dynamic> webtoons = jsonDecode(response.body);
       return webtoons.map((webtoon) => WebtoonModel.fromJson(webtoon)).toList();
+    }
+
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonDetailById(String id) async {
+    final uri = Uri.parse("$baseUrl/$id");
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
     }
 
     throw Error();
