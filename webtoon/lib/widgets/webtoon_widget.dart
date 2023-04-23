af51d400b24847adbd9eb323272f92a09423c417
yoon <leeyt1201@gmail.com>
2023-04-23 09:15:31 +0900

    feat: implemented detail screen using Hero

--- webtoon/lib/widgets/webtoon_widget.dart
+++ webtoon/lib/widgets/webtoon_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webtoon/screens/detail_screen.dart';
 
 class Webtoon extends StatelessWidget {
   final String id, title, thumb;
@@ -10,23 +11,33 @@ class Webtoon extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        print('take me home');
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                DetailScreen(id: id, title: title, thumb: thumb),
+            // fullscreenDialog: true,
+          ),
+        );
       },
       child: Column(
         children: [
-          Container(
-            height: 250,
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 15,
-                    offset: const Offset(10, 10),
-                    color: Colors.black.withOpacity(0.5),
-                  ),
-                ]),
-            child: Image.network(thumb),
+          Hero(
+            tag: id,
+            child: Container(
+              height: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 15,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.5),
+                    ),
+                  ]),
+              child: Image.network(thumb),
+            ),
           ),
           const SizedBox(
             height: 10,
